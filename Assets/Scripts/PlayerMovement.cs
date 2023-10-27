3598d1e9371b90d3f51180182b1d97c80ba77ebf
Adam's PC <ajdupuy15@gmail.com>
2023-10-27 15:16:41 -0500

    basic scene for testing crated and basic physics added to player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed = 8f;
+    [SerializeField] private float JumpHeight = 10f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * MoveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpHeight);
+        }
+    }
+}
